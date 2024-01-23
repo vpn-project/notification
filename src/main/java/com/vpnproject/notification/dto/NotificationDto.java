549a23238809473f039efc53d4e0c395a41b0f21
ZloyEgor <elopatov74@outlook.com>
2024-01-24 01:45:30 +0300

    feat: kafka works!

--- src/main/java/com/vpnproject/notification/dto/NotificationDto.java
+++ src/main/java/com/vpnproject/notification/dto/NotificationDto.java
@@ -1,8 +1,12 @@
 package com.vpnproject.notification.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationDto {
     private String description;
 }
