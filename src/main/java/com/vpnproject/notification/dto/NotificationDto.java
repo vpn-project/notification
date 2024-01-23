d285c79eb8684ba710e148b3b02367799712c973
ZloyEgor <elopatov74@outlook.com>
2024-01-24 01:57:44 +0300

    feat: change notification dto

--- src/main/java/com/vpnproject/notification/dto/NotificationDto.java
+++ src/main/java/com/vpnproject/notification/dto/NotificationDto.java
@@ -8,5 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NotificationDto {
-    private String description;
+
+    private String username;
+    private String email;
 }
